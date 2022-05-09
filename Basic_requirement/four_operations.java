b60c51ed096eb55b5b00dc7c99c6ca979ab69bbc
ChaofengWu0 <82070917+ChaofengWu0@users.noreply.github.com>
2022-05-09 23:12:57 +0800

    删写完了

--- Basic_requirement/four_operations.java
+++ Basic_requirement/four_operations.java
@@ -2,6 +2,8 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+// age unit_price
+
 public class FourOperations_version2 {
 
     private static Connection con = null;
@@ -57,13 +59,12 @@ public class FourOperations_version2 {
         String[] titles = new String[rsmd.getColumnCount()];
         System.out.println("Now please input the values you want to insert\n" +
                 "If you don't want to insert the value,please input null,else input the values");
-//        ArrayList<String> titles = new ArrayList<>();
         boolean[] has_value = new boolean[rsmd.getColumnCount()];
         String[] inserts = new String[rsmd.getColumnCount()];
         for (int i = 0; i < titles.length; i++) {
             String now_title = rsmd.getColumnName(i + 1);
             titles[i] = now_title;
-            System.out.print(now_title + " ");
+            System.out.print(now_title + "     ");
         }
         System.out.println();
 
@@ -128,9 +129,18 @@ public class FourOperations_version2 {
         System.out.println("Please input the table name you want to delete");
         String tale_name = sc.next();
         Statement statement = con.createStatement();
-        String sql = "delete from ";
-
-
+        ArrayList<String> tempTitles = new ArrayList<>();
+        getTitles(tale_name, tempTitles);
+        StringBuilder sql = new StringBuilder("delete from ");
+        sql.append(tale_name);
+        String[] constrains = new String[1];
+        constrains[0] = null;
+        getConstraints(tempTitles, constrains);
+        sql.append(" ");
+        sql.append(constrains[0]);
+        statement.execute(sql.toString());
+        con.commit();
+        if (statement != null) statement.close();
     }
 
     private static void update() {
@@ -141,13 +151,14 @@ public class FourOperations_version2 {
 
     }
 
+
     private static void updateForCenter(String center, String name, Connection con) throws SQLException {
         String check_ = "select director from center where center = ?";
         PreparedStatement preparedStatement1 = con.prepareStatement(check_);
         preparedStatement1.setString(1, center);
         ResultSet resultSet = preparedStatement1.executeQuery();
         resultSet.next();
-        if (resultSet.getRow()!=0){
+        if (resultSet.getRow() != 0) {
             return;
         }
         String update = "update center set director = ? where center = ?";
@@ -158,33 +169,76 @@ public class FourOperations_version2 {
         con.commit();
     }
 
-    private static void getConstraints(String[] tempTitles, String table_name, String[] constrains) {
+    private static void getConstraints(ArrayList<String> tempTitles, String[] constrains) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> arrayList = new ArrayList<>();
         System.out.println("Please choose the constrains\n" +
                 "If the input is null,it means that this attribute doesn't have a constraint\n" +
-                "If the the type of column is 'age' 'unit_price',please input two numbers a,b meaning [a,b]\n" +
-                "Otherwise, please input a string to satisfy your attempt.");
-        if (!table_name.equals("model")) {
-            // 这里可以获得总共要输入的数量
-            int total = 0;
-            boolean has_age = false;
-            for (int i = 0; i < tempTitles.length; i++) {
-                System.out.print(tempTitles[i] + "   ");
-                if (tempTitles[i].equals("age")) {
-                    total += 2;
-                    has_age = true;
-                } else total++;
+                "If the the type of column is 'age' 'unit_price' 'purchase_price' 'quantity',please input two numbers a,b meaning [a,b]\n" +
+                "Otherwise, please input a string to satisfy your attempt.\n" +
+                "And please input stop to finish the input");
+        // 我要做什么？ 我要给where后面添加语句。
+        // 怎么添加？ 当有限制的时候就要添加。
+        // 怎么判断有没有限制？ 用has_value来确定此处有没有限制,如果has_value对应的索引值是true，那么说明此值有限制,否则无限制，如果一个限制都没有，那么直接返回即可
+        // 具体的值应该怎么添加？ 如果是varchar类型，直接where column = value即可，如果是integer类型, between and
+//        ArrayList<Boolean> has_value = new ArrayList<>();
+        ArrayList<String> values = new ArrayList<>();
+        for (String tempTitle : tempTitles) {
+            System.out.print(tempTitle + "     ");
+        }
+        System.out.println();
+        String temp;
+        int cnt_for_title = 0;
+        int cnt = 0;
+        while (!(temp = sc.next()).equals("stop")) {
+            if (temp.equals("null")) {
+                cnt_for_title++;
+                continue;
             }
-            ArrayList<String> inputs = new ArrayList<>();
-            for (int i = 0; i < total; i++) {
-                inputs.add(sc.next());
+            arrayList.add(tempTitles.get(cnt_for_title));
+            values.add(temp);
+            cnt_for_title++;
+            cnt++;
+        }
+        int value_cnt = 0;
+        for (int i = 0; i < cnt; i++) {
+            if (i == 0) {
+                constrains[0] = "where ";
             }
-
-
-        } else {
-            // 0是product
-            // 1是model
-
+            // 说明是integer类型的
+            constrains[0] += tempTitles.get(i);
+            if (check(tempTitles.get(i))) {
+                constrains[0] += " between ";
+                constrains[0] += values.get(value_cnt++);
+                constrains[0] += " and ";
+                constrains[0] += values.get(value_cnt++);
+            } else {
+                constrains[0] += " = ";
+                constrains[0] += "'";
+                constrains[0] += values.get(value_cnt++);
+                constrains[0] += "' ";
+            }
+            if (i != cnt - 1) {
+                constrains[0] += " and";
+            } else {
+                constrains[0] += ";";
+            }
         }
     }
+
+    private static void getTitles(String table_name, ArrayList<String> tempTitles) throws SQLException {
+        String sql = "select * from ";
+        sql += table_name;
+        Statement statement = con.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        ResultSetMetaData rsmd = resultSet.getMetaData();
+        for (int i = 0; i < rsmd.getColumnCount(); i++) {
+            tempTitles.add(rsmd.getColumnName(i + 1));
+        }
+    }
+
+    private static boolean check(String s) {
+        return (s.equals("age") || s.equals("purchase_price") || s.equals("quantity") || s.equals("unit_price"));
+    }
+
 }
