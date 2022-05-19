855af464cc657c4465459f2aadad8e5c24020148
ChaofengWu0 <82070917+ChaofengWu0@users.noreply.github.com>
2022-05-19 14:01:06 +0800

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -146,14 +146,73 @@ create table advanced_store
     name     varchar(100) primary key,
     quantity integer not null
 );
+-- 设计情景：
+-- 一个USER manager 可以增删查改staff,需要一个人来专门管理员工，他自然可以对员工表拥有所有的权限
+-- 一个USER supplier，只可以增store里面的数据，这是一个供应商，那么他只能向库存里面添加货物，不能对库存进行其他的操作
+-- 一个USER visitor，只可以查enterprise表,这是一个访客用户，他只能对enterprise进行查操作来了解我们公司的合作伙伴
+-- 一个USER salesman,可以增改order_table，这是销售人员的账户，他只能增加订单，修改订单，不能做其他的事情
+create role manager with
+    login
+    nosuperuser
+    noreplication
+    password '123456';
+COMMENT ON ROLE manager is 'It has all permissions on staff';
+grant all on staff to manager;
 
-create table user_table
-(
-    id   integer primary key,
-    name varchar(100),
-    pwd  varchar(100),
-    type varchar(100)
-);
+
+create role supplier with
+    login
+    nosuperuser
+    noreplication
+    password '123456';
+COMMENT ON ROLE manager is 'It has insert permission on store';
+grant insert on store to supplier;
+
+
+create role visitor with
+    login
+    nosuperuser
+    noreplication
+    password '123456';
+COMMENT ON ROLE visitor is 'It has select permission on enterprise';
+grant select on enterprise to visitor;
+
+create role salesman with
+    login
+    nosuperuser
+    noreplication
+    password '123456';
+COMMENT ON ROLE salesman is 'It has insert and update permissions on order_table';
+grant insert, update on order_table to salesman;
+
+
+
+-- 设计情景：
+-- 有一张验证表,只有验证成功才能注册成为管理者
+
+-- 有一张用户表，用来记录用户的信息
+-- 有一个CEO（type）,可以增删查改所有的表，可以修改验证表,但是修改之后，不会对之前验证过的用户权限造成影响
+-- 有管理者 manager（type），唯一功能：删除普通用户,可以申请
+-- 有一堆ordinary用户，只能select center表，product表，model表
+-- 只有manager和ordinary可以申请注册，如果重复申请，不成立，报错。
+--
+-- create table identification
+-- (
+--     id serial primary key ,
+--     identity_code char(4)
+-- );
+--
+-- create
+--     table user_table
+-- (
+--     id   serial primary key,
+--     name varchar(100) unique ,
+--     pwd  varchar(100),
+--     type varchar(100)
+-- );
+
+
+
 
 
 
